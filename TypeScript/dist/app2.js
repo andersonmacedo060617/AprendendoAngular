f6cc23d8436c697e7cd8c46978e565552d384710
Anderson Macedo <dasilvamacedo.anderson@gmail.com>
2018-11-28 23:39:48 -0200

    Inicio Aula 17

---
+++ TypeScript/dist/app2.js
@@ -0,0 +1,17 @@
+"use strict";
+var Carro = /** @class */ (function () {
+    function Carro() {
+        this.velocidade = 0;
+    }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
+    return Carro;
+}());
+var car = new Carro();
