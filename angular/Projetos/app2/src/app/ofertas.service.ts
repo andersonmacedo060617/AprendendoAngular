24ea8729e7cf04e7d771ecdebfdb79a19c2882d8
Anderson Macedo <dasilvamacedo.anderson@gmail.com>
2018-12-17 08:25:36 -0200

    commit

--- angular/Projetos/app2/src/app/ofertas.service.ts
+++ angular/Projetos/app2/src/app/ofertas.service.ts
@@ -1,89 +1,51 @@
 import { Oferta } from './shared/oferta.model';
+import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
 
+import 'rxjs/add/operator/toPromise'
+
+@Injectable()
 export class OfertaService{
 
-    public ofertas : Array<Oferta> = [ 
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/1/img1.jpg"},
-                {url: "/assets/ofertas/1/img2.jpg"},
-                {url: "/assets/ofertas/1/img3.jpg"},
-                {url: "/assets/ofertas/1/img4.jpg"}
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/2/img1.jpg"},
-                {url: "/assets/ofertas/2/img2.jpg"},
-                {url: "/assets/ofertas/2/img3.jpg"},
-                {url: "/assets/ofertas/2/img4.jpg"}
-            ]
-        
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Estância das águas",
-            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/3/img1.jpg"},
-                {url: "/assets/ofertas/3/img2.jpg"},
-                {url: "/assets/ofertas/3/img3.jpg"},
-                {url: "/assets/ofertas/3/img4.jpg"},
-                {url: "/assets/ofertas/3/img5.jpg"},
-                {url: "/assets/ofertas/3/img6.jpg"}
-            ]
-        }
-    ]
+    constructor(private http: HttpClient){}
 
-    public getOfertas():Array<Oferta>{
-        return this.ofertas
-    }
+    public ofertas : Array<Oferta>
 
-    public getOfertas2():Promise<Array<Oferta>>{
-        return new Promise((resolve, reject)=>{
-            let deu_certo = true
-            if(deu_certo){
-                setTimeout(()=>resolve(this.ofertas), 3000)
-            }else{
-                reject({
-                    codigo_erro: 404, 
-                    message_erro: 'Servidor não encontado' 
-                })
-            }
-            
-            
-        })
-        .then((ofertas: Oferta[])=>{
-            console.log('primeiro then')
-            return ofertas
-        })
-        .then((ofertas : Oferta[])=>{
-            console.log('Segundo Then')
-            return new Promise((resolve2, reject2)=>{
-                setTimeout(()=>{ resolve2( ofertas )},3000)
+    public getOfertas():Promise<Oferta[]>{
+        return this.http.get<Oferta[]>('http://localhost:300/ofertas').subscribe()
+            .toPromise()
+            .then((resposta: any)=>{
+                resposta.json()
             })
-        })
-        .then((ofertas:Oferta[])=>{
-            console.log('Terceiro then executado porque estava aguardando uma promisse ser resolvida')
-            return ofertas
-        })
     }
+
+    // public getOfertas2():Promise<Array<Oferta>>{
+    //     return new Promise((resolve, reject)=>{
+    //         let deu_certo = true
+    //         if(deu_certo){
+    //             setTimeout(()=>resolve(this.ofertas), 3000)
+    //         }else{
+    //             reject({
+    //                 codigo_erro: 404, 
+    //                 message_erro: 'Servidor não encontado' 
+    //             })
+    //         }
+            
+            
+    //     })
+    //     .then((ofertas: Oferta[])=>{
+    //         console.log('primeiro then')
+    //         return ofertas
+    //     })
+    //     .then((ofertas : Oferta[])=>{
+    //         console.log('Segundo Then')
+    //         return new Promise((resolve2, reject2)=>{
+    //             setTimeout(()=>{ resolve2( ofertas )},3000)
+    //         })
+    //     })
+    //     .then((ofertas:Oferta[])=>{
+    //         console.log('Terceiro then executado porque estava aguardando uma promisse ser resolvida')
+    //         return ofertas
+    //     })
+    // }
 }
