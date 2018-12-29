d74539ec66538961929b1e7611582cd8b159bb20
Anderson Macedo <dasilvamacedo.anderson@gmail.com>
2018-12-28 22:22:14 -0200

    commit

--- angular/Projetos/app2/src/app/oferta/oferta.component.ts
+++ angular/Projetos/app2/src/app/oferta/oferta.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertaService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { Observable, interval, observable, Observer } from 'rxjs';
+import 'rxjs';
+
 
 @Component({
   selector: 'app-oferta',
@@ -22,6 +25,39 @@ export class OfertaComponent implements OnInit  {
       .then((oferta:Oferta)=>{
         this.oferta = oferta
       })
+
+    // observable observavel
+    let meuObservableTeste = Observable.create((observer: Observer<number>)=>{
+      observer.next(1)
+      observer.next(2)
+      observer.next(3)
+      observer.complete()
+      observer.next(4)
+    })
+
+    //observable observador
+    meuObservableTeste.subscribe(
+      (resultado: any)=> console.log(resultado + 10),
+      (erro:string)=>console.log(erro),
+      ()=> console.log('stream foi finalizada')
+    )
+
+    let tempo = interval(2000)
+    tempo.subscribe((intervalo:number)=>{
+      console.log(intervalo)
+    })
+
+    /*
+    this.route.params.subscribe((parametro:any)=>{
+      console.log(parametro)
+    },
+    (erro:any)=>{
+      console.log(erro)
+    },
+    ()=>{
+      console.log('Processamento foi classificado como concluido!')
+    })*/
+
   }
 
 }
