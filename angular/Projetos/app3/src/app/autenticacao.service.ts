ad7062fd134a8e8b363a0c20167ccec424e4ea68
Anderson Macedo <anderson.macedo@thomsonreuters.com>
2019-02-22 18:08:26 -0300

    commit

--- angular/Projetos/app3/src/app/autenticacao.service.ts
+++ angular/Projetos/app3/src/app/autenticacao.service.ts
@@ -3,7 +3,7 @@ import * as firebase from 'firebase'
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
-@Injectable()
+
 export class AutenticacaoService{
 
     public token_id:string
